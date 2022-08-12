216d4b2be5db2806cf2dd7f90d39e458ae5bfbe5
tuinnguyen <nnminh.sam.1803@gmail.com>
2022-08-12 12:36:48 +0700

    Upload new problems

---
+++ prob/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(0);cin.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO
+
+    freopen("data.txt", "r", stdin);
+
+    
+    
+    return 0;
+}
